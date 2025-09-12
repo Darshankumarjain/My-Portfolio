0c0d9ecbf10f8ac7dc8379d5b116c3746439024c
DARSHANKUMAR JAIN <145105070+Darshankumarjain@users.noreply.github.com>
2025-09-12 11:16:54 +0530

    .

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-const texts = ["Frontend Developer", "Python Developer"];
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-let isDeleting = false;
+// const texts = ["Frontend Developer", "Python Developer"];
+// let count = 0;
+// let index = 0;
+// let currentText = "";
+// let letter = "";
+// let isDeleting = false;
 
-function typeEffect() {
-  currentText = texts[count];
+// function typeEffect() {
+//   currentText = texts[count];
   
-  if (isDeleting) {
-    letter = currentText.slice(0, --index);
-  } else {
-    letter = currentText.slice(0, ++index);
-  }
+//   if (isDeleting) {
+//     letter = currentText.slice(0, --index);
+//   } else {
+//     letter = currentText.slice(0, ++index);
+//   }
 
-  document.querySelector(".section__text__p2").textContent = letter;
+//   document.querySelector(".section__text__p2").textContent = letter;
 
-  if (!isDeleting && letter.length === currentText.length) {
-    isDeleting = true;
-    setTimeout(typeEffect, 1000); // pause before deleting
-  } else if (isDeleting && letter.length === 0) {
-    isDeleting = false;
-    count = (count + 1) % texts.length; // move to next word
-    setTimeout(typeEffect, 500); // pause before typing next word
-  } else {
-    setTimeout(typeEffect, isDeleting ? 100 : 150); // speed
-  }
-}
+//   if (!isDeleting && letter.length === currentText.length) {
+//     isDeleting = true;
+//     setTimeout(typeEffect, 1000); // pause before deleting
+//   } else if (isDeleting && letter.length === 0) {
+//     isDeleting = false;
+//     count = (count + 1) % texts.length; // move to next word
+//     setTimeout(typeEffect, 500); // pause before typing next word
+//   } else {
+//     setTimeout(typeEffect, isDeleting ? 100 : 150); // speed
+//   }
+// }
 
-typeEffect();
+// typeEffect();
